50d61501943c5c72ff3da560ed3d09f1bf772ad1
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-08-01 21:47:53 +0530

    topic arrays start

---
+++ .vscode/arrays/arrays.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class arrays {
+    public static void main(String args[]){
+
+    // syntex to creating arrays 
+
+    // int marks[]=new int [50];
+    // int  numbers[] = {1,2,3};
+    // String name[]={"pankaj "," Rajan "," Dheeraj "};// ese create karte hai array ko 
+
+    // input/output 
+    int marks[] = new int [40];
+    Scanner sc = new Scanner(System.in);
+
+   // System.out.println("enter your marks ");
+
+//     marks[0]=sc.nextInt();
+//     marks[1]=sc.nextInt();
+//     marks[2]=sc.nextInt();
+// System.out.println();
+//     System.out.println(marks[0]);
+//     System.out.println(marks[1]);
+//     System.out.println(marks[2]);
+
+
+// arrays length 
+
+System.out.println("array length "+marks.length);
+
+
+    
+}
+}
