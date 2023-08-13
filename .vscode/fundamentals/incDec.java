e22019a0faa5806bc3bc70e51daabe1c07bb79d8
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-08-14 00:09:38 +0530

    update fundamentals by genie ashwani

---
+++ .vscode/fundamentals/incDec.java
@@ -0,0 +1,10 @@
+public class incDec {
+    public static void main(String args[]){
+        int a =10;
+
+        // System.out.println(a-=5);
+        System.out.println(a*=5);
+       // System.out.println((--a+--a)*(++a-a--)+(a+++--a)*(++a+a--));
+    }
+    
+}
