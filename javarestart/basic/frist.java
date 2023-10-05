36011629714e962ff5b5228ad0e0edafdf81536e
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-10-05 10:11:25 +0530

    cpp remove

---
+++ javarestart/basic/frist.java
@@ -0,0 +1,10 @@
+
+package javarestart.basic;
+import java.util.*;
+public class frist {
+    public static void main(String [] args){
+    System.out.println("Radhe krishn ");
+    System.out.println("Hare krishna ");
+   
+}
+}
