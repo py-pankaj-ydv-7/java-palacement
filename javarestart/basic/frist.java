10d4563e24761d91358868813f6bb0af93776a44
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-10-05 10:13:37 +0530

    Restart coding journey

--- javarestart/basic/frist.java
+++ javarestart/basic/frist.java
@@ -5,6 +5,7 @@ public class frist {
     public static void main(String [] args){
     System.out.println("Radhe krishn ");
     System.out.println("Hare krishna ");
+    System.out.println("this is my frist program , while i am restarting my coding journey ");
    
 }
 }
