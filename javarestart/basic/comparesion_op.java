23e52b3df581344b965ea1c40a3334db78747194
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-10-08 01:49:30 +0530

    comparesion_op

---
+++ javarestart/basic/comparesion_op.java
@@ -0,0 +1,38 @@
+package javarestart.basic;
+
+public class comparesion_op {
+    public static void main(String [] args){
+
+        System.out.println("----------------/==/-----------------");
+        int a = 10;
+        int b =10;
+        System.out.println(10==10);
+        System.out.println(a==b);
+
+        System.out.println("----------------/!=/-----------------");
+        System.out.println(10!=10);
+        System.out.println(9!=10);
+
+        System.out.println("----------------/>/-----------------");
+        System.out.println(100>10);
+        System.out.println(9>10);
+
+
+        System.out.println("------------/</--------------");
+		System.out.println(100<10);
+		System.out.println(9<10);
+
+		System.out.println("------------/>=/--------------");
+		System.out.println(33>=32);
+		System.out.println(33>=33);
+		System.out.println(33>=45);
+
+		System.out.println("------------/<=/--------------");
+		System.out.println(33<=32);
+		System.out.println(33<=33);
+		System.out.println(33<=45);
+
+
+    }
+    
+}
