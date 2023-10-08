63d6ce2a992a2e1a6623c50d5cf9d4c14c221542
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-10-08 12:04:51 +0530

    BitwiseProgram

---
+++ javarestart/basic/BitwiseOperator.java
@@ -0,0 +1,12 @@
+package javarestart.basic;
+
+public class BitwiseOperator {
+    public static void main(String [] args){
+
+        int a=5;
+        int b=2;
+        System.out.println(a|b);
+        System.out.println(a&b);
+    }
+    
+}
