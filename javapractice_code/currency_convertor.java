c1f7d15abfd2a466e2c9ac28d84f6bf943a5cdab
Pankaj Yadav <pankajyadav851685@gmail.com>
2023-11-15 09:24:04 +0530

    currency convertor

---
+++ javapractice_code/currency_convertor.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class currency_convertor {
+public static void main(String args []){
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println(" 1 dollar = 85 indian rupee ");
+    int dollar = 80;
+    System.out.println("enter a amount ");
+    int ammount =sc.nextInt();
+    System.out.println("indian rupee convert into us dollar "+ammount*dollar);
+}
+    
+}
